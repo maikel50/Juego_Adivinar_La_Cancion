b63aa16989905b7bd52f4d96cac25976e79c4bb7
maikel50 <palango4887@gmail.com>
2024-02-01 13:34:10 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Juego_Adivinar_La_Cancion"
+include(":app")
+ 
