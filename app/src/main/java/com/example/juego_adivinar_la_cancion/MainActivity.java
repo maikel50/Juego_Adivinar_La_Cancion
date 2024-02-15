9c28f4c0b1faa2e63969b7ce4320f472788c11dd
maikel50 <palango4887@gmail.com>
2024-02-15 11:11:32 +0100

    cambios en main activity

--- app/src/main/java/com/example/juego_adivinar_la_cancion/MainActivity.java
+++ app/src/main/java/com/example/juego_adivinar_la_cancion/MainActivity.java
@@ -8,13 +8,14 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,18 +23,26 @@ Button btnPlay,btnReplay,btnStop,btnEnviar;
 RadioButton opcion1,opcion2,opcion3;
 MediaPlayer media;
 private final int[] listaAudios = {R.raw.audio1, R.raw.audio2};
+
+ArrayList<String> nombresPortadas = new ArrayList<>();
+Boolean acierto1 = false, acierto2 = false;
 private final int REQUEST_CODE_PERMISSIONS = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnPlay=findViewById(R.id.btnPlay);
         btnReplay=findViewById(R.id.btnReplay);
         btnStop=findViewById(R.id.btnStop);
         btnEnviar=findViewById(R.id.btnEnviar);
+
         opcion1=findViewById(R.id.opcion1);
         opcion2=findViewById(R.id.opcion2);
         opcion3=findViewById(R.id.opcion3);
+
+        nombresPortadas.add("portada1");
+        nombresPortadas.add("portada2");
 
         if(ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
@@ -47,6 +56,8 @@ private final int REQUEST_CODE_PERMISSIONS = 1000;
                     },REQUEST_CODE_PERMISSIONS);
         }
 
+        String nombrePortada = cambiarPortadaAleatoriamente(nombresPortadas);
+        cambiarOpcion(nombrePortada);
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,6 +68,7 @@ private final int REQUEST_CODE_PERMISSIONS = 1000;
             @Override
             public void onClick(View view) {
                 playLocal(view);
+
             }
         });
         btnReplay.setOnClickListener(new View.OnClickListener() {
@@ -68,17 +80,51 @@ private final int REQUEST_CODE_PERMISSIONS = 1000;
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cambiarPortadaAleatoriamente();
-                cambiarCancionAleatoriamente();
+                String nombrePortada = cambiarPortadaAleatoriamente(nombresPortadas);
+                if(nombrePortada.equals("portada1")){
+                    cambiarOpcion(nombrePortada);
+                    if(opcion1.isChecked()){
+                        cambiarCancionAleatoriamente();
+                        eliminarCancionActual();
+                        nombresPortadas.remove(nombrePortada);
+                    }
+                }else  if(nombrePortada.equals("portada2")){
+                    cambiarOpcion(nombrePortada);
+                    if(opcion2.isChecked()){
+                        cambiarCancionAleatoriamente();
+                        eliminarCancionActual();
+                        nombresPortadas.remove(nombrePortada);
+
+                    }
+                }
+                Toast.makeText(MainActivity.this, "Se ha pulsado el botón Enviar", Toast.LENGTH_SHORT).show();
+
             }
         });
+
     }
-    private void cambiarPortadaAleatoriamente() {
-        int numeroAleatorio = new Random().nextInt(2) + 1;
-        String nombreImagen = "portada" + numeroAleatorio;
+    private String cambiarPortadaAleatoriamente(ArrayList<String> nombresPortadas) {
+        Random random = new Random();
+        int indiceAleatorio = random.nextInt(nombresPortadas.size());
+        String nombreImagen = nombresPortadas.get(indiceAleatorio);
+
         int idImagen = getResources().getIdentifier(nombreImagen, "drawable", getPackageName());
         ImageView imageViewPortada = findViewById(R.id.imagen);
         imageViewPortada.setImageResource(idImagen);
+
+        return nombreImagen;
+    }
+    private void cambiarOpcion(String nombresPortadas) {
+
+        if(nombresPortadas.equals("portada1")){
+            opcion1.setText("Bichiyal");
+            opcion2.setText("La Zona");
+            opcion3.setText("Soy Peor");
+        }else if(nombresPortadas.equals("portada2")){
+            opcion1.setText("Party");
+            opcion2.setText("Moscow Mule");
+            opcion3.setText("Amanece");
+        }
     }
     private void cambiarCancionAleatoriamente() {
         if (media != null) {
@@ -89,7 +135,17 @@ private final int REQUEST_CODE_PERMISSIONS = 1000;
 
         media = MediaPlayer.create(this, idAudio);
     }
+    private void eliminarCancionActual() {
+        int cancionActual = media != null ? media.getAudioSessionId() : -1;
 
+        for (int i = 0; i < listaAudios.length; i++) {
+            if (listaAudios[i] == cancionActual) {
+
+                listaAudios[i] = 0;
+                break;
+            }
+        }
+    }
     public void playLocal(View view) {
         try {
             if (media == null) {
